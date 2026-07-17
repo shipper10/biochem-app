d1c510d706ac0d63cee9d52e90bef430d0e38e6c
shipper10 <87245353+shipper10@users.noreply.github.com>
2026-07-17 21:48:29 +0200

    Convert capacitor.config.ts to capacitor.config.json

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.oraby.biochemqa',
-  appName: 'Oraby Biochemistry QA',
-  webDir: 'www'
-};
-
-export default config;
