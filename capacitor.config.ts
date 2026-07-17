fbe5d24c6821447e42991b2ab0d36da6fe738242
shipper10 <shipper10@example.com>
2026-07-17 21:23:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oraby.biochemqa',
+  appName: 'Oraby Biochemistry QA',
+  webDir: 'www'
+};
+
+export default config;
